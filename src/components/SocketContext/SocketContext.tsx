ed59d8a556a8432a8d2de9b7b0fd94584f1bb330
legt <legt1407@gmail.com>
2023-08-06 14:22:22 -0400

    update cors

--- src/components/SocketContext/SocketContext.tsx
+++ src/components/SocketContext/SocketContext.tsx
@@ -2,17 +2,19 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-const socket:any = io(`${import.meta.env.VITE_CONNECTION}`);
+const socket: any = io(`https://chattio-backend.onrender.com`, {
+  withCredentials: true
+});
 const SocketContext = createContext(socket);
 
-const SocketProvider = ({ children }:any) => {
+const SocketProvider = ({ children }: any) => {
   return (
     <>
-      <SocketContext.Provider value={ {socket} }>
+      <SocketContext.Provider value={{ socket }}>
         {children}
       </SocketContext.Provider>
     </>
   );
 };
 
-export {SocketProvider, SocketContext}
+export { SocketProvider, SocketContext };
